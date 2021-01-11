fe310810cbca9911676573ecf460b0db863cc95e
Edresson <edresson1@gmail.com>
2021-01-11 08:41:32 -0300

    update demo audios

---
+++ audios_demo/copy-audios.sh
@@ -0,0 +1,5 @@
+nohup cp -r ~/GPU-Moacir/voice-cloning/zero-shot/MOS-sentences/Residual-11-speakers/vctk_setences/ ./Res-11-speakers/  &
+nohup cp -r ~/GPU-Moacir/voice-cloning/zero-shot/MOS-sentences/Residual/vctk_setences/ ./Res/  &
+nohup cp -r ~/GPU-Moacir/voice-cloning/zero-shot/MOS-sentences/Gated/vctk_setences/ ./Gated/  &
+nohup cp -r ~/GPU-Moacir/voice-cloning/zero-shot/MOS-sentences/Transformers/vctk_setences/ ./Trans/  &
+nohup cp -r ~/GPU-Moacir/voice-cloning/zero-shot/MOS-sentences/Tacotron/vctk_setences/ ./Tacotron/  &
